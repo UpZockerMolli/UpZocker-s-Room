26203c5fc5a62aae1932e508adc0f7e97ddf9132
UpZockerMolli <o.mildner1987@outlook.de>
2026-01-14 15:58:07 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,70 +1,38 @@
 const express = require("express");
 const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
-const users = {};
+let users = {};
 
 io.on("connection", socket => {
 
-    // ===== JOIN =====
     socket.on("join", username => {
         users[socket.id] = username;
-
-        // vorhandene Nutzer senden
-        const others = Object.keys(users).filter(id => id !== socket.id);
-        socket.emit("existing users", others);
-
-        // neuen Nutzer ankündigen
+        socket.emit("existing users", Object.keys(users).filter(id=>id!==socket.id));
         socket.broadcast.emit("new user", socket.id);
     });
 
-    // ===== CHAT =====
-    socket.on("chat message", msg => {
-        io.emit("chat message", msg);
-    });
+    socket.on("chat message", msg => { io.emit("chat message", msg); });
 
-    // ===== VIDEO OFFER =====
-    socket.on("video offer", data => {
-        io.to(data.to).emit("video offer", {
-            offer: data.offer,
-            from: socket.id,
-            username: data.username
-        });
-    });
+    socket.on("join video", username => { users[socket.id] = username; });
 
-    // ===== VIDEO ANSWER =====
-    socket.on("video answer", data => {
-        io.to(data.to).emit("video answer", {
-            answer: data.answer,
-            from: socket.id
-        });
-    });
+    socket.on("video offer", data => { io.to(data.to).emit("video offer",{ offer: data.offer, from: socket.id }); });
 
-    // ===== ICE =====
-    socket.on("ice candidate", data => {
-        io.to(data.to).emit("ice candidate", {
-            candidate: data.candidate,
-            from: socket.id
-        });
-    });
+    socket.on("video answer", data => { io.to(data.to).emit("video answer",{ answer: data.answer, from: socket.id }); });
 
-    // ===== DISCONNECT =====
+    socket.on("ice candidate", data => { io.to(data.to).emit("ice candidate",{ candidate: data.candidate, from: socket.id }); });
+
     socket.on("disconnect", () => {
         const name = users[socket.id];
-
-        socket.broadcast.emit("user disconnected", socket.id, name);
+        if(name) io.emit("chat message", `👋 ${name} hat den Videochat verlassen`);
         delete users[socket.id];
     });
 
 });
 
-server.listen(PORT, () => {
-    console.log("Server läuft auf Port", PORT);
-});
+server.listen(PORT, () => console.log(`Server läuft auf http://localhost:${PORT}`));
